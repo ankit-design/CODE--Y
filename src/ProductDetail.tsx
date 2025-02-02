e383fc43901bebc891e8fef64f57b34c50df6435
Ankit <nauriyalankit511@gmail.com>
2025-02-03 00:05:15 +0530

    added back  icon to the code

--- src/ProductDetail.tsx
+++ src/ProductDetail.tsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { HiArrowCircleLeft } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
+
 
 export function ProductDetail({data}) {
   const[value,setValue]=useState(0);
@@ -16,16 +19,23 @@ export function ProductDetail({data}) {
     console.log("useEffect action !! ");
   },[])
 
- 
+//  function handleclick(){
+//    window.history.go(-1);
+//  }
   
   return (
-    <div className=' w-full '>
+    <div className=' w-full  flex flex-row '>
+      <Link to="/" >
+      <HiArrowCircleLeft className='text-5xl'  />
+      </Link>
+      <div>
         <img className='w-1/2 h-2/4  object-cover mt-2 ml-2' src={data.imgUrl} alt='image' />
         <h1>{data.category}</h1>
         <h2>${data.price}</h2>
         <p>{data.title}</p>
-        <input placeholder="Quantity..."value={value} type='number' onChange={handleInputChange}></input>
-        <button onClick={handleChange}>Add to cart</button>
+        <input placeholder="Quantity..."value={value} type='number' onChange={handleInputChange} className=' border-solid border-4 mr-2'></input>
+        <button onClick={handleChange} className=' border-solid border-4 ' >Add to cart</button>
+        </div>
     </div>
   )
 }

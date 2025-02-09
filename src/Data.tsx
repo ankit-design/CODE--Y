2b89bc25dcd38426d9d9846a702160df53321c0c
Ankit <nauriyalankit511@gmail.com>
2025-02-09 15:13:48 +0530

    Used .then in place of try catch in data call

--- src/Data.tsx
+++ src/Data.tsx
@@ -7,12 +7,18 @@ import axios from 'axios';
  
 export async function dataProvider(){
    
-    try{
-   return (await axios.get('https://dummyjson.com/products')).data.products;
-   }catch(err){
-    console.log("the err is :-> ",err);
+await axios.get('https://dummyjson.com/products')
+.then((res)=>{
+    return res.data.products;
+})
+.catch((err)=>{
+    console.log("the erroe is :-> ",err);
     return [];
-   }
+})
+.finally(()=>{
+    console.log("API call working !!! ");
+})
+   
 }
 
 
